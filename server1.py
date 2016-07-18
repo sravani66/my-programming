d5f7eae0b03883592f4dd1255630ff87ef9288ad
sravani66 <sravanichowdary66@gmail.com>
2016-07-18 12:32:19 +0530

    Add files via upload

---
+++ server1.py
@@ -0,0 +1,14 @@
+import socket
+
+HOST = 'chitturi'                 # Symbolic name meaning all available interfaces
+PORT = 50007              # Arbitrary non-privileged port
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('chitturi', 50007))
+s.listen(1)
+conn, addr = s.accept()
+#print 'Connected by', addr
+while 1:
+    data = conn.recv(1027)
+    if not data: break
+    conn.send(data)
+conn.close()
